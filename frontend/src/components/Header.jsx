03d5184a98193651c5b3fa081df522ce455077f0
Murlidhar108 <soni48438@gmail.com>
2024-03-19 19:13:15 +0530

    create and run the server

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <>
       <header className='bg-slate-200 shadow-lg'>
-        <div className='flex justify-between items-center mx-w-6xl mx-auto p-3 '>
+        <div className='flex justify-between items-center mx-auto mx-w-6xl p-3'>  
           
             <h1 className='font-bold text-md flex flex-wrap:wrap' >
               <span className='text-pink-500'>Murlidhar</span>
@@ -17,13 +17,13 @@ export default function Header() {
             </Link>
             </h1>
 
-          <form className='flex bg-slate-100 p-3 rounded-3xl items-center' >
+          <form className=' bg-slate-100 p-3 rounded-3xl flex items-center' >
             <input type="text" placeholder='search here ...' className='bg-transparent focus:outline-none
             width-25 sm:w-65' />
             <FaSearch className='text-blue-400'/>
           </form>
 
-          <ul className='flex space-x-4 sm:flex-row sm:items-center sm:ml-auto'>
+          <ul className='flex gap-4'>
             <Link to='/home' ><li className='hover:underline text-slate-700'>Home</li></Link>
             <Link to='/about' ><li className='hover:underline text-slate-700'>About</li></Link>
             <Link to='/sign-in' ><li className='hover:underline text-slate-700'>Signin</li></Link>
@@ -33,6 +33,8 @@ export default function Header() {
     </>
   )
 }
+
+
 
 
 
