56c7818e44c32ced39bbf731d4b8629f3ca924bd
Murlidhar108 <soni48438@gmail.com>
2024-03-19 20:24:38 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 4000;
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config(); // initialising 
+
+
+// const url = process.env.MONGO;
+
+mongoose.connect('mongodb://localhost:27017/real-estate').then(()=>{
+    console.log("mongo db connected");
+}).catch((err) => {
+    console.log(err);
+})
+
+// console.log('MongoDB URI:', process.env.MONGO);
 
 app.listen(port , () => {
     console.log('backend is running');
 })
+
+
+
+
+
+
